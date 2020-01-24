47793132a589d1a4fdbcae15cd250c4abb1c8307
msinkec <mihael@sinkec.xyz>
2020-01-24 20:15:13 +0100

    Title now shown again, config changes.

--- config.h
+++ config.h
@@ -1,11 +1,11 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int gappx     = 0;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
+static const int topbar             = 0;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Mononoki:style=Bold:pixelsize=14:antialias=true:autohint=true" };
 static const char dmenufont[]       = "Mononoki:style=Bold:pixelsize=14:antialias=true:autohint=true" ;
 static const char col_gray1[]       = "#222222";
@@ -14,11 +14,12 @@ static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
 static const char col_violet[]      = "#7d03a6";
-static const char col_white[]      = "#ffffff";
+static const char col_white[]       = "#ffffff";
+static const char col_aqua[]        = "#00ffff";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_cyan, col_gray1, col_cyan },
+	[SchemeSel]  = { col_aqua, col_gray1, col_aqua },
 };
 
 /* tagging */
@@ -35,7 +36,7 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
+static const float mfact     = 0.50; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 
